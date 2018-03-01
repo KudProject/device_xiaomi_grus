9becc5702dbf460c56f0d7b4ee8946d702e6bb5b
Ruchi Kandoi <kandoiruchi@google.com>
2018-03-01 14:09:08 -0800

    grus: Remove system and vendor image dependency
    libnfc-nci.conf shouldn't be a part of vendor image, since it is
    accessed by libnfc-nci and Nfc application, both are part of system
    image.
    
    Test: Nfc enable/disable
    Bug: 72080121
    Change-Id: I03b6387afc35a521c8a6f9fea1ec2e649d56a38f
    Signed-off-by: Albert I <kras@raphielgang.org>

--- device.mk
+++ device.mk
@@ -47,7 +47,7 @@ PRODUCT_PACKAGES += \
     nqnfcse_access.xml
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # Inherit from proprietary version
