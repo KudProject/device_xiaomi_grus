12bb58cf09b9a5662dbe84e1cd17280e1c7d290c
blunden <blunden2@gmail.com>
2018-04-24 21:58:27 +0200

    grus: Disable proximity sensor check for dialer's answer UI
    This feature doesn't work reliably with grus's elliptic sensor.
    
    Change-Id: Idfc531a1d981eba66118f0e1749a23db520480cf
    Signed-off-by: Albert I <kras@raphielgang.org>

--- device.mk
+++ device.mk
@@ -17,7 +17,8 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-dot
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
