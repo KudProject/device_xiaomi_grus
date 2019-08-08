0f7462ce59244834b4b7d4e97b195b779ae91514
Albert I <kras@raphielgang.org>
2019-08-09 04:27:33 +0800

    grus: Define system, boot and dtbo partition sizes
    Signed-off-by: Albert I <kras@raphielgang.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,5 +12,10 @@ DEVICE_PATH := device/xiaomi/grus
 # Assert
 TARGET_OTA_ASSERT_DEVICE := grus
 
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 134217728
+BOARD_DTBOIMG_PARTITION_SIZE := 25165824
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3758096384
+
 # Inherit from the proprietary version
 -include vendor/xiaomi/grus/BoardConfigVendor.mk
