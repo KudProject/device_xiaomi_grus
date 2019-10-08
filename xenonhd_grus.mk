f31fecf76a00e194b68b50c0a06b3a4210f1df9b
Albert I <kras@raphielgang.org>
2019-10-09 00:54:47 +0800

    grus: Update build fingerprint from V10.3.4.0.PFBMIXM
    Latest changes in SafetyNet CTS now requires platform security patch to
    match as well, so set it properly here as well.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- xenonhd_grus.mk
+++ xenonhd_grus.mk
@@ -19,10 +19,14 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="grus-user 9 PKQ1.181121.001 V10.3.3.0.PFBCNXM release-keys"
+# Platform security patch
+# NOTE: Only bump this with appropriate build fingerprint
+PLATFORM_SECURITY_PATCH := 2019-08-01
 
-BUILD_FINGERPRINT := "Xiaomi/grus/grus:9/PKQ1.181121.001/V10.3.3.0.PFBCNXM:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="grus-user 9 PKQ1.181121.001 V10.3.4.0.PFBMIXM release-keys"
+
+BUILD_FINGERPRINT := "Xiaomi/grus/grus:9/PKQ1.181121.001/V10.3.4.0.PFBMIXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="grus" \
