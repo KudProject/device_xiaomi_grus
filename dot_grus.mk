f3f3770a4e9c678e58ddc05b746b3af3bc82edcf
Albert I <kras@raphielgang.org>
2021-05-28 17:15:48 +0800

    grus: Use vendor init to set product model, name and build description
    Signed-off-by: Albert I <kras@raphielgang.org>

--- dot_grus.mk
+++ dot_grus.mk
@@ -13,13 +13,8 @@ $(call inherit-product, vendor/dot/config/common.mk)
 PRODUCT_NAME := dot_grus
 PRODUCT_DEVICE := grus
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := MI 9 SE
 PRODUCT_MANUFACTURER := Xiaomi
 
 BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="grus-user 10 QKQ1.190828.002 V12.0.4.0.QFBMIXM release-keys" \
-    PRODUCT_NAME="grus"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
